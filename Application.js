731f5c38dfefc5f840d715b5e2be173cdf148e87
onlineabedini <onlineabedini@gmail.com>
2021-10-29 16:14:29 +0330

    telegramBot running on server

--- Application.js
+++ Application.js
@@ -1,10 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+
 const mongoose = require('mongoose')
-const {startBot} = require('./bot/index')
+const { startBot } = require('./bot/index')
 
 class Application {
     constructor() {
         this.configApp()
         this.setupMongo()
+        this.expressConfig()
         startBot()
     }
     setupMongo() {
@@ -17,7 +23,13 @@ class Application {
     configApp() {
         require("dotenv").config();
     }
+    expressConfig() {
+        app.listen(port, () => {})
+
+        app.get('/', (req, res) => {
+            res.send('Hello World!')
+        })
+    }
 }
 
 module.exports = Application
-
