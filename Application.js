f33066d83f5e9651606203b6485fa1d3834bd82c
onlineabedini <onlineabedini@gmail.com>
2021-11-03 00:42:37 +0330

    deploy code added

--- Application.js
+++ Application.js
@@ -3,6 +3,12 @@ const app = express();
 const port = 5000;
 
 
+// const { MongoClient } = require('mongodb');
+// const uri = "mongodb+srv://onlineadmin:p0S6YEUsJhbrVbAn@cluster0.gramm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+
 const mongoose = require('mongoose')
 const { startBot } = require('./bot/index')
 
@@ -14,11 +20,12 @@ class Application {
         startBot()
     }
     setupMongo() {
-        mongoose.connect('mongodb://localhost:27017/RadeGozineBot').then(() => {
-            console.log('db connected');
-        }).catch(err => {
-            console.log(err)
-        })
+        mongoose.connect('mongodb://localhost:27017/RadeGozineBot')
+            // client.connect(err => {
+            //     const collection = client.db("test").collection("devices");
+            //     // perform actions on the collection object
+            //     client.close();
+            // });
     }
     configApp() {
         require("dotenv").config();
