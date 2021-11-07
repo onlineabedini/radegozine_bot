90d38998d8494c9599e831e6200281c8a6ee268d
onlineabedini <onlineabedini@gmail.com>
2021-11-07 12:49:25 +0330

    server mongo added

--- Application.js
+++ Application.js
@@ -8,7 +8,8 @@ class Application {
         startBot()
     }
     setupMongo() {
-        mongoose.connect('mongodb://localhost:27017/RadeGozineBot').then(() => {
+        mongoose.connect('mongodb://127.0.0.1:27017/RadeGozineBot').then(() => {
+            // mongoose.connect('mongodb://localhost:27017/RadeGozineBot').then(() => {
             console.log('db connected');
         }).catch(err => {
             console.log(err)
