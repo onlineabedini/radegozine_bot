9768c283ac92a9120eb68dc5817c2f0e76ff3f29
onlineabedini <onlineabedini@gmail.com>
2021-11-07 11:52:43 +0330

    last changes

--- Application.js
+++ Application.js
@@ -1,14 +1,3 @@
-const express = require('express');
-const app = express();
-const port = 5000;
-
-
-// const { MongoClient } = require('mongodb');
-// const uri = "mongodb+srv://onlineadmin:p0S6YEUsJhbrVbAn@cluster0.gramm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-
-
 const mongoose = require('mongoose')
 const { startBot } = require('./bot/index')
 
@@ -16,26 +5,13 @@ class Application {
     constructor() {
         this.configApp()
         this.setupMongo()
-        this.expressConfig()
         startBot()
     }
     setupMongo() {
         mongoose.connect('mongodb://localhost:27017/RadeGozineBot')
-            // client.connect(err => {
-            //     const collection = client.db("test").collection("devices");
-            //     // perform actions on the collection object
-            //     client.close();
-            // });
     }
     configApp() {
         require("dotenv").config();
-    }
-    expressConfig() {
-        app.listen(port, () => {})
-
-        app.get('/', (req, res) => {
-            res.send('Hello World!')
-        })
     }
 }
 
